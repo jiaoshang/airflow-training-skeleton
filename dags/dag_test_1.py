20b799e247e6c946798d4b48ae39dff06d383b88
Chongze Jiao <cjiao@bol.com>
2019-12-09 11:45:09 +0100

    set schedule interval daily

--- dags/dag_test_1.py
+++ dags/dag_test_1.py
@@ -10,7 +10,7 @@ args = {
 dag = DAG(
     dag_id='dag_test_1',
     default_args=args,
-    schedule_interval=None,
+    schedule_interval='@daily',
 )
 
 t1 = DummyOperator(task_id="task1", dag=dag)
