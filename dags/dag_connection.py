ff25db50dd557028ea62ba8020c5d85ff155d654
Chongze Jiao <cjiao@bol.com>
2019-12-09 15:56:30 +0100

    subselect on transfer date

--- dags/dag_connection.py
+++ dags/dag_connection.py
@@ -9,15 +9,19 @@ args = {
 }
 
 
+sql = '''SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = {{ds}} '''
+
+
 with DAG(
         dag_id='dag_exercise_connection',
         default_args=args,
         schedule_interval=None,
 ) as dag:
     query_table = PostgresToGoogleCloudStorageOperator(task_id="query_table",
-                                                       sql='''SELECT transfer_date FROM land_registry_price_paid_uk LIMIT 2''',
+                                                       sql=sql,
                                                        postgres_conn_id="postgres_training",
-                                                       bucket='dag-exercise-connection', filename="test_table")
+                                                       bucket='dag-exercise-connection',
+                                                       filename="test_table")
 
 
 query_table
