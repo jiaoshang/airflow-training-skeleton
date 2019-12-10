2803d75e169c9a9f70afdd9b822fdb8686d837c8
Chongze Jiao <cjiao@bol.com>
2019-12-10 16:28:36 +0100

    none http_connect_id

--- dags/dag_connection.py
+++ dags/dag_connection.py
@@ -23,6 +23,7 @@ with DAG(
                                                        filename="test_table/ {{ds}}.json")
 
     fetch_exchange_rate = HttpToGcsOperator(task_id="fetch_exchange_rate",
+                                            http_conn_id=None,
                                             endpoint=http_endpoint,
                                             gcs_bucket="dag-exercise-connection",
                                             gcs_path="exchange_rate")
